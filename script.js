61bc681439835fd8113f4ce234be90f9a75fb3b2
Zalt <126626310+Zalt12@users.noreply.github.com>
2023-08-26 21:14:40 -0500

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,8 @@
 
 colors=("\\e[31m" "\\e[32m" "\\e[33m" "\\e[34m" "\\e[35m" "\\e[36m")  # List of colors
 reset_color="\\e[0m"  # Reset color code
-
 while true; do
     color=${colors[$((RANDOM % ${#colors[@]}))]}  # Pick a random color from the list
-    echo -e "$color Hello, Linux! $reset_color"   # Print the colored message
+    echo -en "\r$color Hello, Linux! $reset_color"   # Print the colored message
     sleep 1  # Pause for a moment before changing color
 done
